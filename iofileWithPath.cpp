fb7cedef68f6575e22868a95185efef95d3815b7
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:43:50 +0700

    jika file ada maka

--- iofileWithPath.cpp
+++ iofileWithPath.cpp
@@ -36,5 +36,11 @@ int main(){
     infile.open(namaFile +".txt");
 
     cout << endl << ">= Membuka dan membaca file" << endl;
- 
+    // jika file ada maka
+    if (infile.is_open())
+    {
+  
+    
+    }
+
 }
