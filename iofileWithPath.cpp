afe2c944bf24912574ff12e7692c230a736d9990
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:37:56 +0700

    unlimited loop untuk menulis

--- iofileWithPath.cpp
+++ iofileWithPath.cpp
@@ -16,3 +16,16 @@ int main(){
     outfile.open(namaFile +".txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true){
+        cout << "-";
+        //mendapatakan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke  dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
