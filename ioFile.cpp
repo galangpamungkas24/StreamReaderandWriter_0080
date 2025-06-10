c4dc13e4aaa3c0aa16addf30c21d8e64fb796bc0
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:16:46 +0700

    oprasi file dalam mode membaca

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,3 +20,22 @@
     }
     //selesai dalam menulis sekarang tutup filenya
     outfile.close(); 
+
+    //operasi file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file untuk membuka
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile,baris))
+        {
+            //dan tampilka disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
