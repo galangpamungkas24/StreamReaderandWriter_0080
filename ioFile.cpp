d75aed61ba0ca5e1e47f227f5acddce2d0ed9215
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:12:22 +0700

    oprasi file dalam mode nulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -1,9 +1,4 @@
- #include <fstream>
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
+ int main(){
     string baris;
 
     //operasi file dalam mode nulis.
@@ -12,39 +7,3 @@ int main(){
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-    //unlimited loop untuk menulis
-    while (true){
-        cout << "-";
-        //mendapatakan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //loop akan berhenti jika anda memasukkan karakter q
-        if (baris == "q") break;
-        // menulis dan memasukan nilai dari 'baris' ke  dalam file
-        outfile << baris << endl;
-    }
-    //selesai dalam menulis sekarang tutup filenya
-    outfile.close();
-
-    //operasi file dalam mode membaca
-    ifstream infile;
-    //menunjuk ke sebuah file untuk membuka
-    infile.open("contohfile.txt");
-
-    cout << endl << ">= Membuka dan membaca file" << endl;
-    // jika file ada maka
-    if (infile.is_open())
-    {
-        //melakukan perulangan setiap baris
-        while (getline(infile,baris))
-        {
-            //dan tampilka disini
-            cout << baris << '\n';
-        }
-        //tutup file tersebut setelah selesai
-        infile.close();
-    }
-    //jika tidak ditemukan file maka akan menampilkan ini
-    else cout << "unable to open file";
-    return 0;
-}
