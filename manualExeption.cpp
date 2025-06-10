1df531cbc9ed6f1bd91f302c3abc064fad54086b
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:33:41 +0700

    melempar sebuah interger

---
+++ manualExeption.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+using namespace std;
+
+int main(){
+    try {
+        cout << "selamat belajar di Ti umy" << endl;
+        throw 0,5; //melempar sebuah interger maka
+        cout << "pernyataan tidak akan di eksekusi" << endl;
+    }
+    catch (int a){
+        //blok ini akan di eksekusi
+        cout << "pengecualian akan di eksekusi" << endl;
+    }
+    catch (...) {
+        /jika selain integer maka blok ini akan di eksekusi/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
