c56d21829f649b5a135bf0b4b572c3db954794f8
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 15:36:55 +0700

    oprasi file dalam mode nulis

---
+++ iofileWithPath.cpp
@@ -0,0 +1,18 @@
+ #include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namaFile;
+
+    cout << "masukan nama file: ";
+    cin >> namaFile;
+
+    //operasi file dalam mode nulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file untuk membuka
+    outfile.open(namaFile +".txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
